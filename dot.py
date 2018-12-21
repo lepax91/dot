1fc557fc1dc29001cd1dd5476f2d6076b474cd72
lepax91 <44847340+lepax91@users.noreply.github.com>
2018-12-21 17:58:06 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -98,19 +98,7 @@ async def off():
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    await client.change_presence(game=discord.Game(name='.help | with '+str(len(set(client.get_all_members())))+' users', type=1))
-
-@client.command(pass_context=True)
-async def tweet(ctx, usernamename:str, *, txt:str):
-    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get(url) as r:
-            res = await r.json()
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-            embed.set_image(url=res['message'])
-            embed.title = "{} twitted: {}".format(usernamename, txt)
-            await client.say(embed=embed)					
+    await client.change_presence(game=discord.Game(name='.help | with '+str(len(set(client.get_all_members())))+' users', type=1))					
 								
 @client.command()
 async def lewdkitsune():
