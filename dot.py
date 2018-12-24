96ec456eca06c27ae6bb6c5bf72e610735010dff
lepax91 <44847340+lepax91@users.noreply.github.com>
2018-12-24 08:48:48 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -53,9 +53,9 @@ async def help(ctx):
       await client.add_reaction(dmmessage, reaction1)
       await client.add_reaction(dmmessage, reaction2)
       await client.add_reaction(dmmessage, reaction3)      
-      await client.say('<:a_:524648895796740126> | What are you waiting for, just look at DMs..')
+      embed.add_field(name='Stop waiting..', value= '<:a_:524648895796740126> | What are you waiting for, just look at DMs')
      
-
+     
 @client.event
 async def on_reaction_add(reaction, user):
   if reaction.message.server is None:
