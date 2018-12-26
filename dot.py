73b894ea988386944b7c1f0557a96c2d34440899
lepax91 <44847340+lepax91@users.noreply.github.com>
2018-12-26 15:08:33 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -31,7 +31,7 @@ def is_owner(ctx):
  
 @client.command()
 async def help():
-        embed = discord.Embed(title="Help — Fun, Music, Moderation!", description="Dot is a simply bot with any commands", color=0x9b9dff) 
+        embed = discord.Embed(title="Help — Fun, Music, Moderation!", description="Dot is a simply bot with any commands!", color=0x9b9dff) 
         embed.add_field(name="Fun", value="`ping`, `meme`, `avatar`,  `serverinfo`, `love`, `fortnite`, `penis`, `woof`, `meow`, `hug`,`kiss`, `howgay`", inline=False)
         embed.add_field(name="Moderation", value="`ban`, `warn`, `say`, `clear`", inline=False)
         embed.add_field(name="Music", value="`play`, `skip`, `stop`, `song`, `queue`, `volume`, `pause`, `resume`, `mutemusic`, `umutemusic`", inline=False)
