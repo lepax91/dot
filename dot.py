66070574fce9990616a61b1e82ac6df4fe955a2c
lepax91 <44847340+lepax91@users.noreply.github.com>
2018-12-27 11:22:55 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -353,11 +353,12 @@ async def help():
         embed = discord.Embed(title="Help — Fun, Music, Moderation!", description=" <:Avatar:527564140051562517> | Dot is a simply bot with any commands!", color=0x9b9dff)
         embed.add_field(name="Information:", value="`help`, `info`", inline=False)
         embed.add_field(name="Fun:", value="`ping`, `meme`, `avatar`,  `serverinfo`, `love`, `fortnite`, `penis`, `woof`, `meow`, `hug`,`kiss`, `howgay`, `animemes`", inline=False)
+        embed.add_field(name="Memes:", value="`meme`, `animemes`", inline=False)
         embed.add_field(name="Moderation:", value="`ban`, `warn`, `say`, `clear`", inline=False)
         embed.add_field(name="Music:", value="`play`, `skip`, `stop`, `song`, `queue`, `volume`, `resume`, `mutemusic`, `umutemusic`", inline=False)
-        embed.set_footer(text=f'Total 28 Commands, 4 Category | Dot — Alpha 1.0')
+        embed.set_footer(text=f'Total 28 Commands, 5 Category | Dot — Alpha 1.0')
         await client.say(embed=embed)
-   
+	
 
 @client.command(pass_context = True)
 async def animemes(ctx):
