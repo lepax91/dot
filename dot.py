d708d5c968deda31450e3dc98dc85c50142b4061
lepax91 <44847340+lepax91@users.noreply.github.com>
2018-12-27 11:48:23 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -351,7 +351,7 @@ async def penis(ctx):
 async def help():	
         embed = discord.Embed(title="Help — Fun, Music, Moderation!", description=" <:Avatar:527564140051562517> | Dot is a simply bot with any commands!", color=0x9b9dff)
         embed.add_field(name="**Information:**", value="`help`, `info`", inline=False)
-        embed.add_field(name="**Fun:**", value="`ping`, `meme`, `avatar`,  `serverinfo`, `love`, `fortnite`, `penis`, `woof`, `meow`, `hug`,`kiss`, `howgay`, `animemes`", inline=False)
+        embed.add_field(name="**Fun:**", value="`ping`, `meme`, `avatar`,  `serverinfo`, `love`, `fortnite`, `penis`, `woof`, `meow`, `hug`, `kiss`, `howgay`", inline=False)
         embed.add_field(name="**NSFW:**", value="`hentai`", inline=False)
         embed.add_field(name="**Moderation:**", value="`ban`, `warn`, `say`, `clear`", inline=False)
         embed.add_field(name="**Music:**", value="`play`, `skip`, `stop`, `song`, `queue`, `volume`, `resume`, `mutemusic`, `umutemusic`", inline=False)      
