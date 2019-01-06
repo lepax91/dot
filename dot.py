7494581416c450bbef6fa0abe89b936bd82ac93c
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-06 21:44:34 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -41,7 +41,7 @@ client = discord.Client()
 client = commands.Bot(command_prefix=get_prefix)
 client.remove_command('help')
 #()  []  {} `
-init_extensions = ['cogs.fun']
+init_extensions = ['cogs.images']
 
 
 if __name__ == '__main__':
