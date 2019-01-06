2b65fdbe8fdeddc8ef81e5d8bd730c841caca1ca
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-06 21:34:34 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -41,7 +41,7 @@ client = discord.Client()
 client = commands.Bot(command_prefix=get_prefix)
 client.remove_command('help')
 #()  []  {} `
-init_extensions = ['cogs.Music']
+init_extensions = ['cogs.fun']
 
 
 if __name__ == '__main__':
