d4d5abde9e38c5d52302cd41064ed5ec019eea80
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-17 16:20:03 +0100

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,13 +1,6 @@
 import asyncio
 import discord
 from discord.ext import commands
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
 my_queue = {}
 def __init__(self, bot):
         self.bot = bot
