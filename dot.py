0ee59114068a42aa68f96e093adecb8bd716e796
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-18 19:40:02 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -69,7 +69,7 @@ async def info():
 async def on_ready():
     print("The bot is ready!")
     print("Connected on " + str(len(client.servers)) + " servers:") 
-    await client.change_presence(game=discord.Game(name='.help | with '+str(len(set(client.get_all_members())))+' users', status=discord.Status.idle))
+    await client.change_presence(game=discord.Game(name='.help | with '+str(len(set(client.get_all_members())))+' users', url="twitch.com/myname", type=1))
 						
 						    				
 
