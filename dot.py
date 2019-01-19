7eab8549d0738e4387e13ee44851d92c5e82170c
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-19 21:33:39 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -108,21 +108,6 @@ async def coinflip(ctx):
     embed.set_image(url=random.choice(choices))
     await client.say(embed=embed)
 
-@client.command(pass_context=True, no_pm=True, aliases=["Shibe"])
-async def shibe(ctx):
-	await client.send_typing(ctx.message.channel)
-	try:
-		url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false"
-		response = requests.get(url)
-		data = json.loads(response.text)
-		embed=discord.Embed(color=discord.Color((r << 16) + (g << 8) + b))
-		embed.set_author(name="Here is your Shibe {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
-		embed.set_image(url=data[0])
-		await client.say(embed=embed)
-	except:
-		x = await client.say("Sorry, there was an error with the **shibe** command")
-		await asyncio.sleep(5)
-		await client.delete_message(x)
 
 @client.command(pass_context=True)  
 @commands.has_permissions(ban_members=True)     
