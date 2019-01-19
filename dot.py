1831b24a8c422e512752bcec8c0a2dbb47a90ff4
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-19 15:49:29 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -100,17 +100,6 @@ async def shiba(ctx):
 		embed.set_image(url = data["image"])
 		await client.say(embed=embed)						
 	
-@client.command(pass_context=True)
-async def fox(ctx):
-	await client.send_typing(ctx.message.channel)
-	try:
-		url = "https://randomfox.ca/floof"
-		response = requests.get(url)
-		data = json.loads(response.text)
-		embed=discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-		embed.set_author(name =  "Here's Your Fox {}".format(ctx.message.author.name), icon_url = ctx.message.author.avatar_url)
-		embed.set_image(url = data["image"])
-		await client.say(embed=embed)
 	
 @client.command(pass_context=True)
 async def quit(ctx):
@@ -461,7 +450,7 @@ async def help():
         embed.add_field(name=":closed_lock_with_key: **Developer Commands:**", value="`quit`, `emojiids`", inline=False)
         embed.add_field(name=":printer: **Internet Commands:**", value="`wiki`, `urban`", inline=False)	
         embed.add_field(name="<:FeelsHappyHugMan:535141367475863563> **Fun:**", value="`love`, `fortnite`, `penis`, `hug`, `kiss`, `howgay`, `rps`, `coinflip`", inline=False)
-        embed.add_field(name=":cat: **Animals:**", value="`woof`, `meow`, `shibe`, `fox`, `bird`", inline=False)    
+        embed.add_field(name=":cat: **Animals:**", value="`woof`, `meow`, `shiba`", inline=False)    
         embed.add_field(name="<:2109_yikes:535142625129267231> **Memes:**", value="`meme`, `cz_memes`", inline=False)	
         embed.add_field(name=":underage: **NSFW:**", value="`hentai`, `butt`, `cosplay`", inline=False)
         embed.add_field(name="<:4206_lmaolancer2:535143040835125298> **Memes with Fun:**", value="`deepfry`, `text`, `czech`, `rotate`, `isthisa`, `phone`, `thatsmile`, `birthcontrol`, `moe`, `religion`, `disability`", inline=False)
