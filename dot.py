86e6a842055546a38139837e177ba8c3edcc3036
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-20 20:48:50 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -480,14 +480,14 @@ async def aww(ctx):
             embed.timestamp = datetime.datetime.utcnow()
             await client.say(embed=embed)
 
-@client.command(pass_context = True, aliases=["cats"])
+@client.command(pass_context = True)
 async def cat(ctx):
-    colour = '0x' + '007000'
+    colour = '0x' + 'ff00d0'
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://aws.random.cat/meow") as r:
+        async with session.get("https://api.reddit.com/r/cat/random") as r:
             data = await r.json()
-            embed = discord.Embed(title='', description='', color=0x0000FF)
-            embed.set_image(url=data[0]["file"])
+            embed = discord.Embed(title='', description='', color=discord.Color(int(colour, base=16)))
+            embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
             embed.timestamp = datetime.datetime.utcnow()
             await client.say(embed=embed)
 
