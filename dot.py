ec351bc1b666585fefff9c74915c730516819fc5
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-25 19:35:43 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -89,7 +89,7 @@ async def urban(ctx, *, msg:str=None):
 async def on_ready():
     print("The bot is ready!")
     print("Connected on " + str(len(client.servers)) + " servers:") 
-    await client.change_presence(game=discord.Game(name='big chungus')
+    await client.change_presence(game=discord.Game(name='big chungus'))
 							
 @client.command(pass_context=True,no_pm=True)
 async def quit(ctx):
