fb65ca1594ff5e685fa7f5f4b6e3bb42fc725141
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 21:04:51 +0100

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -29,12 +29,7 @@ class Fun:
 			mgs.append(x)
 		await self.bot.delete_messages(mgs)
 		await self.bot.say(":put_litter_in_its_place: x"+str(number-1))
-	
-	@commands.command(pass_context=True,no_pm=True,aliases = ["8ball","magickakoule","choose"])
-	@commands.cooldown(rate=1, per=2, type=commands.BucketType.user)
-	async def eightball(self,ctx):
-		msg = eightballer.eightball_pick()
-		await self.bot.say(msg)
+		
 	@commands.command(pass_context=True,no_pm=True)
 	@commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
 	async def vtip(self,ctx):
