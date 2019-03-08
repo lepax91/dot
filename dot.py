6ee69b4c05877615cd67fbbf744e8e2da2bd96cd
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-08 23:09:56 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -210,7 +210,7 @@ async def help(ctx):
         embed.add_field(name=":printer: **Internet Commands** [3]", value="`wiki`, `google`, `youtube`", inline=False)	
         embed.add_field(name="<:FeelsHappyHugMan:542781371157053451> **Fun** [6]", value="`penis`, `howgay`, `meme`, `8ball`, `vtip`, `fakt`", inline=False)
         embed.add_field(name=":thinking: **Mention Commands** [4]", value="`hug`, `kiss`, `slap`, `tickle`", inline=False)
-        embed.add_field(name=":cat: **Animals** [4]", value="`dog`, `cat`, `bird`, `aww`", inline=False)    
+        embed.add_field(name=":cat: **Animals** [5]", value="`dog`, `cat`, `bird`, `aww`, `rpanda`", inline=False)    
         embed.add_field(name="🔞 **NSFW** [4]", value="`ass`, `hentai`, `pussy`, `snapchat`", inline=False)	
         embed.add_field(name=" <:4206_lmaolancer:542781373157736458> **Memes with Fun** [3]", value="`deepfry`, `text`, `isthisa`", inline=False)
         embed.add_field(name="<:9175_moderation_hammer:542781370687160330> **Moderation** [7]", value="`ban`, `warn`, `say`, `poll`, `purge`, `kick`, `unban`", inline=False)    
@@ -228,16 +228,6 @@ async def aww(ctx):
             embed.timestamp = datetime.datetime.utcnow()
             await client.say(embed=embed)
 				
-@client.command(pass_context = True,no_pm=True)
-@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-async def bird(ctx):
-    colour = '0x' + 'ff0059'
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.reddit.com/r/birdpics/random") as r:
-            data = await r.json()
-            embed = discord.Embed(title='', description='', color=discord.Color(int(colour, base=16)))
-            embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
-            embed.timestamp = datetime.datetime.utcnow()
  
 
 @client.command(pass_context=True, no_pm=True)
