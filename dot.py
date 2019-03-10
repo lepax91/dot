f6133e025eaa871edcc4433ecc45e9cca13bd6fa
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-10 12:44:06 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -71,6 +71,7 @@ async def info(ctx):
     em.add_field(name="Development of Dot", value=f"<@417403958814965771>")
     em.add_field(name="Pomoc s Dotem", value=f"<@273813194861051907>")
     em.add_field(name="Pozvi Dota, do dalšího pěknýho serveru", value=f"[Zde](https://discordapp.com/oauth2/authorize?client_id={client.user.id}&scope=bot&permissions=268905542)")
+    em.add_field(name="Máš nějaké problémy s Dotem?", value=f"[Zde se připoj do Support Serveru!](https://discord.gg/Q8sWryN)
     em.add_field(name="Nezapomeň ho upvotnout!", value=f"[Zde](https://botlist.space/bot/523787927113826305/upvote)",inline=False)
     em.set_footer(text="Dot | Pre-Alpha 1.2")
     await client.say(embed=em)
