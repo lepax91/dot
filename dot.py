6dcfc3af2921efb6b5963825edf8d3b6f41a5ff2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-15 14:50:17 +0100

    Update dot.py

--- dot.py
+++ dot.py
@@ -201,7 +201,7 @@ async def help(ctx):
         embed.add_field(name=" <:4206_lmaolancer:542781373157736458> **Memes with Fun** [3]", value="`deepfry`, `text`, `isthisa`", inline=False)
         embed.add_field(name="⚒️ Supporting [2]", value="`report`, `suggest`", inline=False)
         embed.add_field(name=":ribbon: **Holiday Commands** [2]", value="`svatek`, `zitra`", inline=False)
-        embed.add_field(name="<:9175_moderation_hammer:542781370687160330> **Moderation** [7]", value="`ban`, `warn`, `say`, `poll`, `purge`, `kick`, `unban`", inline=False)
+        embed.add_field(name="<:9175_moderation_hammer:542781370687160330> **Moderation** [8]", value="`ban`, `warn`, `say`, `poll`, `purge`, `kick`, `unban`, `mute`", inline=False)
         embed.add_field(name="🎵 **Music** [9]", value="`play`, `skip`, `stop`, `song`, `queue`, `volume`, `pause`, `mutemusic`, `unmutemusic`", inline=False)
         embed.set_footer(text=f'Requested: {ctx.message.author.display_name} | Prefix [.]', icon_url=f'{ctx.message.author.avatar_url}')
         await client.say(embed=embed)     
