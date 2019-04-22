2bed2f81eb21ac38f931295fc7ecf293e2204ffd
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-22 16:26:54 +0200

    Update dot.py

--- dot.py
+++ dot.py
@@ -145,13 +145,17 @@ async def icon(ctx):
     embed.set_image(url="{}".format(ctx.message.server.icon_url))
     await client.say(embed=embed)
 	      
-
 @client.command(pass_context=True, no_pm=True)
 async def info(ctx):
+    second = time.time() - start_time
+    minute, second = divmod(second, 60)
+    hour, minute = divmod(minute, 60)
+    day, hour = divmod(hour, 24)		
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     em = discord.Embed(title= "", description="", color = discord.Color((r << 16) + (g << 8) + b))
     em.add_field(name="💻 Servery", value=len(client.servers))
     em.add_field(name="👥 Online Uživatelé", value=str(len({m.id for m in client.get_all_members() if m.status is not discord.Status.offline})))
+    em.add_field(name="🕒 Uptime", value="**%dd %dh %dm %ds**"% (day, hour, minute, second), inline=False)	
     em.add_field(name='🗂️ Kanály', value=f"{sum(1 for g in client.servers for _ in g.channels)}")
     em.add_field(name="📚 Knihovna", value=f"discord.py, discord.js")
     em.add_field(name="<:emoji_3:569883823941222400> Dot Development Project", value=f"<@417403958814965771>, <@273813194861051907>")
@@ -160,4 +164,6 @@ async def info(ctx):
     em.add_field(name="💼 Nezapomeň Dotovy dát Upvote!", value=f"[Zde](https://botlist.space/bot/539139339741954099/upvote)",inline=False)
     em.set_footer(text="Dot | v1.5a")
     await client.say(embed=em)
+
 		                                                                                                
+client.run(TOKEN, client = True)
